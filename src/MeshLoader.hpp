31dc13a78b3d7f56c9dc5d9170df6979d6a08c4b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-12-19 04:09:15 -0500

    Documented MeshLoader

--- src/MeshLoader.hpp
+++ src/MeshLoader.hpp
@@ -12,8 +12,26 @@
 class MeshLoader
 {
 public:
+	/**
+	 * @brief clears any vertices and indicies still stored in the MeshLoader.
+	 */
 	void clear();
+
+	/**
+	 * @brief adds the specified Vertex to the MeshLoader.
+	 * 
+	 * If the specified Vertex already exists in the MeshLoader, it's not repeated. Instead, 
+	 * the Index vector has the Vertex's index appended to it.
+	 * 
+	 * @param vertex the Vertex to add to the MeshLoader.
+	 */
 	void add(const Vertex& vertex);
+
+	/**
+	 * @brief assembles the indices and vertices into an Entity.
+	 * 
+	 * @return Entity the constructed Entity.
+	 */
 	Entity build();
 
 private:
