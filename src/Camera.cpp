412b5e5427b87ecabf2ba35ae245f95d1d14c432
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:13 -0400

    Camera now uses a lookAt vector.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -2,6 +2,8 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <iostream>
+
 Camera::Camera(const glm::vec3& position)
 	: m_position{ position }, m_yaw{ 0 }, m_pitch{ 0 }
 {
@@ -10,5 +12,15 @@ Camera::Camera(const glm::vec3& position)
 
 glm::mat4 Camera::getViewMatrix() const
 {
-	return glm::lookAt(m_position, { 0, 0, 0 }, { 0, 1, 0 });
+	float yaw = m_yaw - 90;
+
+	glm::vec3 front{
+		glm::cos(glm::radians(yaw) * glm::cos(glm::radians(m_pitch))),
+		glm::sin(glm::radians(m_pitch)),
+		glm::sin(glm::radians(yaw) * glm::cos(glm::radians(m_pitch)))
+	};
+
+	front = glm::normalize(front);
+
+	return glm::lookAt(m_position, m_position + front, { 0, 1, 0 });
 }
