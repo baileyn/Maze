dd85bff894808bb74178e9092829581cd674919a
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:11 -0400

    Started adding vertices to draw Cells.

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -1,10 +1,26 @@
 #include "Cell.hpp"
+#include "Vertex.hpp"
 
-float Cell::WIDTH = 2.45f;
-float Cell::HEIGHT = 2.45f;
+#include <vector>
+
+constexpr float HALF_WIDTH = Cell::WIDTH / 2;
+constexpr float HALF_HEIGHT = Cell::HEIGHT / 2;
+
+static std::vector<Vertex> VERTICES = {
+	{ { 0, Cell::HEIGHT, 0 },{ 1, 0, 0 } }, // Front Top Left
+	{ { 0, 0, 0 }, { 1, 0, 0 } }, // Front Bottom Left
+	{ { Cell::WIDTH, Cell::HEIGHT, 0 }, { 1,0,0} }, // Front Top Right
+	{ { Cell::WIDTH, 0, 0 }, { } }, // Front Bottom Right
+};
+
+static std::vector<GLuint> INDICIES = {
+	0, 1, 2,
+	2, 1, 3
+};
 
 Cell::Cell(Type type, unsigned int x, unsigned int y)
-    : m_type{type}, m_x{x}, m_y{y}, m_directions{0b1111}, m_parent{nullptr}
+    : m_type{type}, m_x{x}, m_y{y}, m_directions{0b1111}, m_parent{nullptr},
+	m_vao{}, m_vbo{}, m_ebo{}
 {
 }
 
@@ -26,4 +42,26 @@ Cell* Cell::getParent()
 void Cell::setParent(Cell* parent)
 {
     m_parent = parent;
+}
+
+void Cell::init()
+{
+	glGenVertexArrays(1, &m_vao);
+	glGenBuffers(1, &m_vbo);
+	glGenBuffers(1, &m_ebo);
+
+	glBindVertexArray(m_vao);
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+		glBufferData(GL_EELEMNT_ARRAY_BUFFER, INDICIES.size() * sizeof(INDICIES[0]), INDICIES.data(), GL_STATIC_DRAW);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+		glBufferData(GL_ARRAY_BUFFER, VERTICES.size() * sizeof(VERTICES[0]), VERTICES.data(), GL_STATIC_DRAW);
+	}
+	glBindVertexArray(0);
+}
+
+void Cell::render()
+{
+
 }
--- src/Cell.hpp
+++ src/Cell.hpp
@@ -2,11 +2,13 @@
 #define CELL_HPP
 #pragma once
 
+#include <glad/glad.h>
+
 class Cell
 {
 public:
-    static float WIDTH;
-    static float HEIGHT;
+    static constexpr float WIDTH = 2.45f;
+    static constexpr float HEIGHT = 2.45f;
 
     enum class Type {
         Wall,
@@ -21,12 +23,19 @@ public:
     Cell* getParent();
     void setParent(Cell* parent);
 
+	void init();
+	void render();
+
 private:
     Type m_type;
     unsigned int m_x;
     unsigned int m_y;
     unsigned int m_directions;
     Cell* m_parent;
+
+	GLuint m_vao;
+	GLuint m_vbo;
+	GLuint m_ebo;
 };
 
 #endif // CELL_HPP
