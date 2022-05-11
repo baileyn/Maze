1b6df751b9e5612438b4408a3a7c6d3ad02429e4
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:16 -0400

    Updated an error in CMakeLists and documented the Camera.

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -7,13 +7,44 @@
 class Camera
 {
 public:
+	/**
+	 * The speed that the Camera moves around.
+	 */
 	static constexpr float WALK_SPEED = 10.0f;
+
+	/**
+	 * The speed that the Camera turns.
+	 */
 	static constexpr float TURN_SPEED = 360.0f;
 
+	/**
+	 * Constructs a new Camera at the specified location looking down the -z axis.
+	 * 
+	 * @param position the position that the Camera is currently located.
+	 */
 	Camera(const glm::vec3& position);	
+
+	/**
+	 * Update the internal state of the Camera.
+	 * 
+	 * @param delta the time elapsed since the last update.
+	 */
 	void update(sf::Time& delta);
+
+	/**
+	 * Manually moves the Camera by the specified unites.
+	 * 
+	 * @param x the amount of units to move on the x axis.
+	 * @param y the amount of units to move on the y axis.
+	 * @param z the amount of units to move on the z axis.
+	 */
 	void translate(float x, float y, float z);
 
+	/**
+	 * Constructs and returns the view matrix that represents this Camera.
+	 * 
+	 * @return the view matrix that represents this Camera.
+	 */
 	glm::mat4 getViewMatrix() const;
 
 private:
