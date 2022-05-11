7365ac0bfc1993c45b1707749391aec2ed5eb250
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:17 -0400

    Documented Entity

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -7,9 +7,26 @@
 class Entity
 {
 public:
+	/**
+	 * @brief constructs a new renderable Entity.
+	 * 
+	 * @param indicesCount the number of indices this Entity has.
+	 * @param vao the OpenGL Vertex Array Object.
+	 * @param vbo the OpenGL Vertex Buffer Object.
+	 * @param ebo the OpenGL Element Buffer Object.
+	 */
 	Entity(size_t indicesCount, GLuint vao, GLuint vbo, GLuint ebo);
 
+	/**
+	 * @brief renders this Entity to the screen.
+	 * 
+	 * This function handles binding the appropriate objects and drawing.
+	 */
 	void render();
+
+	/**
+	 * @brief cleans up the OpenGL objects.
+	 */
 	void cleanup();
 
 private:
