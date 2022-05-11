605a5a0a75a5952e09a7f949bf0140a7260b1342
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:17 -0400

    Removed a test function that I didn't intend to commit ever.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,4 @@
 #include "MazeGame.hpp"
-#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
-void addNumber(std::vector<int>& numbers, std::vector<int>& indices, int i)
-{
-	auto location = std::find(std::begin(numbers), std::end(numbers), i);
-
-	if (location == std::end(numbers)) {
-		// If the location wasn't found, append it to the numbers vector
-		numbers.push_back(i);
-
-		// Also add a new entry for this index.
-		indices.push_back(numbers.size() - 1);
-	}
-	else {
-		// Otherwise, if it was found, add the index to the indices
-		indices.push_back(location - std::begin(numbers));
-	}
-}
 
 int main(int argc, char** argv)
 {
